eb8be0dad058050905444842df410c4e165c651d
skferrei <skferrei@insa-toulouse.fr>
2025-12-09 14:23:26 +0100

    project architecture

---
+++ RoutingService/src/test/java/fr/insa/projetIntegrateur/RoutingService/RoutingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.projetIntegrateur.RoutingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
