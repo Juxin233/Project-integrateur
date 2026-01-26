4ba0fa3e6fd9a7fea00ff2a4f3e6cd44cf51a843
malejaj <majimenezh@unal.edu.co>
2026-01-26 13:55:19 +0100

    Cors problem resolu

---
+++ GatewayService/src/main/java/fr/insa/projetIntegrateur/GatewayService/config/CorsFilterConfig.java
@@ -0,0 +1,29 @@
+package fr.insa.projetIntegrateur.GatewayService.config;
+
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsFilterConfig {
+
+  @Bean
+  public CorsFilter corsFilter() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://127.0.0.1:*"));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setExposedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsFilter(source);
+  }
+}
